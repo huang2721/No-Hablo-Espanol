e7ffa1d649fad659e6c4d2be406ea11e96e35854
Michael Huang <huang.2721@osu.edu>
2020-01-08 16:31:30 -0500

    got speech to text working

--- nohablo.py
+++ nohablo.py
@@ -12,16 +12,19 @@ def main():
     language_translator_api_key = 'uHSQ5qFc8bepT-FACslFzxsuD4btr-lnL27RgZwYcMY1'
     language_translator_url = 'https://api.us-east.language-translator.watson.cloud.ibm.com/instances/e25a4ca7-9b06-435d-a187-dea3e5d0030b'
 
+    #speech-to-text
     url = speech_to_text_url + "/v1/recognize"
     headers = {'Content-Type': 'audio/flac'}
     auth = ("apikey", speech_to_text_api_key)
     data = open('./audio-file.flac', 'rb').read()
 
     r = requests.post(url, data =  data , auth = auth , headers = headers)
+    text = r.json()['results'][0]['alternatives'][0]['transcript']
+    
+    #translator
 
-    print(r.text)
 
-
+    
 if __name__ == "__main__":
     main()
 
