4b6e47cd08e4dc355c962d579f9658547b9c7357
Michael Huang <huang.2721@osu.edu>
2020-01-08 16:08:38 -0500

    got post request working for speech to txt

--- nohablo.py
+++ nohablo.py
@@ -4,17 +4,25 @@ import json
 from requests.auth import HTTPBasicAuth
 
 
-text_to_speech_api_key = 'rlYYbQbqwdrzFpndaGRbb8V7q7afmfFxKZ9IBeLQ5r7J'
-text_to_speech_url = 'https://api.us-east.text-to-speech.watson.cloud.ibm.com/instances/0102f85d-c4cd-4cc3-928e-3c7482e01e0f'
-speech_to_text_api_key = 'QO6Rj8oWPbPJtN8cCETL4RsA5WHlDNAVEoWgAT_L33mH'
-speech_to_text_url = 'https://api.us-east.speech-to-text.watson.cloud.ibm.com/instances/88abb65f-7ae0-44fc-bb7f-a88e6d5031ac' 
-language_translator_api_key = 'uHSQ5qFc8bepT-FACslFzxsuD4btr-lnL27RgZwYcMY1'
-language_translator_url = 'https://api.us-east.language-translator.watson.cloud.ibm.com/instances/e25a4ca7-9b06-435d-a187-dea3e5d0030b'
+def main():
+    text_to_speech_api_key = 'rlYYbQbqwdrzFpndaGRbb8V7q7afmfFxKZ9IBeLQ5r7J'
+    text_to_speech_url = 'https://api.us-east.text-to-speech.watson.cloud.ibm.com/instances/0102f85d-c4cd-4cc3-928e-3c7482e01e0f'
+    speech_to_text_api_key = 'QO6Rj8oWPbPJtN8cCETL4RsA5WHlDNAVEoWgAT_L33mH'
+    speech_to_text_url = 'https://api.us-east.speech-to-text.watson.cloud.ibm.com/instances/88abb65f-7ae0-44fc-bb7f-a88e6d5031ac' 
+    language_translator_api_key = 'uHSQ5qFc8bepT-FACslFzxsuD4btr-lnL27RgZwYcMY1'
+    language_translator_url = 'https://api.us-east.language-translator.watson.cloud.ibm.com/instances/e25a4ca7-9b06-435d-a187-dea3e5d0030b'
 
-url = speech_to_text_url + "/v1/recognize"
-headers = {'Content-Type': 'audio/flac'}
-payload = { 'apikey': speech_to_text_api_key , 'data' : sys.argv[0]}
+    url = speech_to_text_url + "/v1/recognize"
+    headers = {'Content-Type': 'audio/flac'}
+    auth = ("apikey", speech_to_text_api_key)
+    data = open('./audio-file.flac', 'rb').read()
 
-r = requests.post(url, data=json.dumps(payload), headers = headers)
+    r = requests.post(url, data =  data , auth = auth , headers = headers)
 
-print(r.text)
+    print(r.text)
+
+
+if __name__ == "__main__":
+    main()
+
+    
